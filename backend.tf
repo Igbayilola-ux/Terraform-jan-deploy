a2e0d48be7835f30e3d70f1f38e4d5efd9c4c429
Igbayilola Adetunji <81198403+Igbayilola-ux@users.noreply.github.com>
2023-09-09 14:50:18 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "my-jan-s3-bucket"
-    key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-jan-dynamodb"
+  backend "local" {
+    path = "relative/path/to/terraform.tfstate"
   }
 }
