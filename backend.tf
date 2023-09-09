d73b904d524bbae098ef67a9e4176b7f6243e08b
Igbayilola Adetunji <81198403+Igbayilola-ux@users.noreply.github.com>
2023-09-09 18:46:23 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,10 @@
 terraform {
   backend "s3" {
     encrypt = false
+    init:
+          extra_args: ["-lock=false"]
+    plan:
+          extra_args: ["-lock=false"]
     bucket = "my-jan-s3-bucket"
     key = "main"
     region = "us-east-2"
